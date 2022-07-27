f35225ce02ed62fb828940698018dd7228e66b6a
AnnaKitou <93220476+AnnaKitou@users.noreply.github.com>
2022-07-27 23:59:21 +0300

    Token

---
+++ CityInfo.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public class AuthenticationRequestBody
+        {
+            public string? UserName { get; set; }
+            public string? Password { get; set; }
+        }
+        public class CityInfoUser
+        {
+            public CityInfoUser(int userId, string userName, 
+                string firstName, string lastName, string city)
+            {
+                UserId = userId;
+                UserName = userName;
+                FirstName = firstName;
+                LastName = lastName;
+                City = city;
+            }
+
+            public int UserId { get; set; }
+            public string UserName { get; set;  }
+            public string FirstName { get; set;  }
+            public string LastName { get; set;  }
+            public string City { get; set;  }
+
+
+        }
+        [HttpPost("authenticate")]
+        public ActionResult<string> Authenticate(
+            AuthenticationRequestBody authenticationRequestBody)
+        {
+            //1. Validate Credentials
+            var user = ValidateUserCredentials(
+                authenticationRequestBody.UserName,
+                authenticationRequestBody.Password);
+            if (user == null)
+            {
+                return  Unauthorized();
+            }
+
+            //2.create a token
+            var securityKey = new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(_configuration["Authentication:SecretForKey"]));
+
+            var signingCredentials = new SigningCredentials(
+               securityKey,SecurityAlgorithms.HmacSha256);
+
+            //Claims
+            var claimsForToken = new List<Claim>();
+            claimsForToken.Add(new Claim("sub",user.UserId.ToString()));
+            claimsForToken.Add(new Claim("given_name", user.FirstName));
+            claimsForToken.Add(new Claim("family_name",user.LastName));
+            claimsForToken.Add(new Claim("city", user.City));
+
+            var jwtSecurityToken = new JwtSecurityToken(
+              _configuration["Authentication:Issuer"] ,
+              _configuration["Authentication:Audience"] ,
+              claimsForToken,
+              DateTime.UtcNow,
+              DateTime.UtcNow.AddHours(1),
+              signingCredentials);
+        }
+
+        private CityInfoUser ValidateUserCredentials(string? userName, string? password)
+        {
+            return new CityInfoUser(1, userName ?? "", "Anna", "Kitou", "Antwerp");
+        }
+    }
+}
