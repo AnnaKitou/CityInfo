9d11b749b4e776ffdca29c051d7d28f3e089660e
AnnaKitou <93220476+AnnaKitou@users.noreply.github.com>
2022-07-30 18:16:25 +0300

    Respose Type

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -43,10 +43,13 @@ namespace CityInfo.API.Controllers
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="id">The id of the city ti get</param>
+        /// <param name="id">The id of the city to get</param>
         /// <param name="includePointsOfInterest">Whether or not points of interest are included</param>
         /// <returns>An IActionResult</returns>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> GetCity(
             int id, bool includePointsOfInterest = false)
         {
